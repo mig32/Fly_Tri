1edea536ab0db4eab3f9709d333d49d87aad871b
g.kaplansky <mig32@list.ru>
2020-04-25 22:23:19 +0300

    map

--- Assets/Maps/Tiles.tsx
+++ Assets/Maps/Tiles.tsx
@@ -3,41 +3,73 @@
  <image source="Tiles.png" width="256" height="256"/>
  <tile id="0">
   <objectgroup draworder="index" id="2">
-   <object id="7" x="0" y="0.189237" width="64.1514" height="63.7729"/>
+   <object id="7" x="0" y="0" width="64" height="64"/>
   </objectgroup>
  </tile>
  <tile id="1">
   <objectgroup draworder="index" id="2">
-   <object id="1" x="0.189237" y="0.189237">
+   <object id="1" x="0" y="0">
     <polygon points="0,0 0,64.1514 63.7729,64.1514"/>
    </object>
   </objectgroup>
  </tile>
  <tile id="2">
   <objectgroup draworder="index" id="2">
-   <object id="1" x="0.0946134" y="-0.283848" width="64.1514" height="63.7729"/>
+   <object id="1" x="0" y="0" width="64" height="64"/>
   </objectgroup>
  </tile>
  <tile id="3">
   <objectgroup draworder="index" id="2">
-   <object id="1" x="0.283851" y="0.0946263" width="64.1514" height="63.7729"/>
+   <object id="1" x="0" y="0" width="64" height="64"/>
   </objectgroup>
  </tile>
  <tile id="4">
   <objectgroup draworder="index" id="2">
-   <object id="1" x="34.6304" y="0">
-    <polygon points="0,0 29.3318,29.521 29.3318,63.9622 -34.4412,63.9622 -34.6304,0.189237"/>
+   <object id="1" x="35" y="0">
+    <polygon points="-0.875,-0.375 29.3318,29.521 29.4568,64.4622 -35.0662,64.2122 -35.8122,0.382419"/>
    </object>
   </objectgroup>
  </tile>
  <tile id="5">
   <objectgroup draworder="index" id="2">
-   <object id="1" x="0.662325" y="0.0946263" width="33.495" height="63.7729"/>
+   <object id="1" x="0" y="0" width="34" height="64"/>
   </objectgroup>
  </tile>
  <tile id="6">
   <objectgroup draworder="index" id="2">
-   <object id="1" x="0.283851" y="26.0201" width="64.1514" height="38.2259"/>
+   <object id="1" x="0" y="26" width="64" height="38"/>
+  </objectgroup>
+ </tile>
+ <tile id="8">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="64.3636" y="30">
+    <polygon points="0,0 0,3.81818 -30.1818,34.1818 -64.1818,34.1818 -64.1818,-29.8182 -30.5455,-30"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="10">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="64" height="64"/>
+  </objectgroup>
+ </tile>
+ <tile id="11">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="64" height="64"/>
+  </objectgroup>
+ </tile>
+ <tile id="13">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="64" height="64"/>
+  </objectgroup>
+ </tile>
+ <tile id="14">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="64" height="64"/>
+  </objectgroup>
+ </tile>
+ <tile id="15">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="64" height="64"/>
   </objectgroup>
  </tile>
 </tileset>
