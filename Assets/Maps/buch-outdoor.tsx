2e3197661e55f934f5f352ec8802c87e613efd3d
g.kaplansky <mig32@list.ru>
2021-09-06 20:14:13 +0300

    new tiles

--- Assets/Maps/buch-outdoor.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.2" tiledversion="1.3.4" name="buch-outdoor" tilewidth="32" tileheight="32" tilecount="72" columns="12">
- <image source="../../TiledMapEditor/examples/buch-outdoor.png" width="384" height="192"/>
-</tileset>
